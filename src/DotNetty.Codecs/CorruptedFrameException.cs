8d77b8a38821698ba054c7098b6db9faff5fcc19
Aaron Stannard <aaron@petabridge.com>
2015-07-16 17:34:42 -0700

    implemented LengthFieldBaseFrameDecoder
    Implemented the following classes:
    - LengthFieldBaseFrameDecoder
    - TooLongFrameException
    - CorruptedFrameException
    
    Added the following projects:
    - DotNetty.Codecs.Tests

---
+++ src/DotNetty.Codecs/CorruptedFrameException.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+namespace DotNetty.Codecs
+{
+    using System;
+
+    /// <summary>
+    /// A <see cref="DecoderException"/> which is thrown when the received frame data could not
+    /// be decoded by an inbound handler.
+    /// </summary>
+    public class CorruptedFrameException : DecoderException
+    {
+        public CorruptedFrameException(string message)
+            : base(message)
+        {
+        }
+
+        public CorruptedFrameException(Exception cause)
+            : base(cause)
+        {
+        }
+    }
+}
