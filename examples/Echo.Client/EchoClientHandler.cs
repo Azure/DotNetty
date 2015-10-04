6e1733a5b1031fce4b709dd3ae21f2f233e99dbb
Bartosz Sypytkowski <b.sypytkowski@gmail.com>
2015-10-04 10:44:48 +0200

    Echo client-server example
    Motivation:
    
    At the present moment documentation of DotNetty is almost non-existent.
    Moreover there are no examples that can be used as starting point for the
    library learning. This commit is mean to provide basic client-server echo example over secure pipeline.
    
    Modification:
    
    - Created an examples directory to be used in the future for possible
      examples projects.
    - Added two console projects used to establish server-client connection
      with basic secure DotNetty echo pipeline.
    
    Result:
    
    New console projects have been created under /examples directory.

---
+++ examples/Echo.Client/EchoClientHandler.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using DotNetty.Buffers;
+using DotNetty.Transport.Channels;
+
+namespace Echo.Client
+{
+    using System;
+    using System.Text;
+    
+    public class EchoClientHandler : ChannelHandlerAdapter
+    {
+        readonly IByteBuffer initialMessage;
+        readonly byte[] buffer;
+
+        public EchoClientHandler()
+        {
+            this.buffer = new byte[EchoClientSettings.Size];
+            this.initialMessage = Unpooled.Buffer(EchoClientSettings.Size);
+            byte[] messageBytes = Encoding.UTF8.GetBytes("Hello world");
+            this.initialMessage.WriteBytes(messageBytes);
+        }
+
+        public override void ChannelActive(IChannelHandlerContext context)
+        {
+            context.WriteAndFlushAsync(this.initialMessage);
+        }
+
+        public override void ChannelRead(IChannelHandlerContext context, object message)
+        {
+            var byteBuffer = message as IByteBuffer;
+            if (byteBuffer != null)
+            {
+                this.buffer.Initialize();
+                byteBuffer.ReadBytes(this.buffer, 0, byteBuffer.ReadableBytes);
+                string msg = Encoding.UTF8.GetString(this.buffer);
+                Console.WriteLine("Received from server: " + msg);
+            }
+            context.WriteAsync(message);
+        }
+
+        public override void ChannelReadComplete(IChannelHandlerContext context)
+        {
+            context.Flush();
+        }
+
+        public override void ExceptionCaught(IChannelHandlerContext context, Exception exception)
+        {
+            context.CloseAsync();
+        }
+    }
+}
---
+++ examples/Echo.Server/EchoServerHandler.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+namespace Echo.Server
+{
+    using System;
+    using DotNetty.Transport.Channels;
+
+    public class EchoServerHandler : ChannelHandlerAdapter
+    {
+        public override void ChannelRead(IChannelHandlerContext context, object message)
+        {
+            context.WriteAsync(message);
+        }
+
+        public override void ChannelReadComplete(IChannelHandlerContext context)
+        {
+            context.Flush();
+        }
+
+        public override void ExceptionCaught(IChannelHandlerContext context, Exception exception)
+        {
+            Console.WriteLine("Exception: " + exception);
+            context.CloseAsync();
+        }
+    }
+}
