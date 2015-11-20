473ec29fcd726f21711d3f5a32be79b321b12347
Sudheer <sudheer594@gmail.com>
2015-11-20 17:13:04 +0530

    Port LoggingHandler
    Port Logging Handler Fixes #38
    
    Motivation:
    
    LoggingHandler  is a must in many situations especially with non-trivial
    pipeline composition so it will definitely be ported soon . It actually
    provides nice looking bytebuf output + additional log for all the
    events.
    
    Modifications:
    Added Logging functionality to DotNetty.Handlers project
    Modified Echo.Server example project to log the events
    
    Result: Easier to learn DotNetty from output by LoggingHandler

---
+++ src/DotNetty.Handlers/Logging/LogLevel.cs
@@ -0,0 +1,29 @@
+﻿namespace DotNetty.Handlers.Logging
+{
+    using DotNetty.Common.Internal.Logging;
+
+    public sealed class LogLevel
+    {
+        public static readonly LogLevel TRACE = new LogLevel(InternalLogLevel.TRACE);
+        public static readonly LogLevel DEBUG = new LogLevel(InternalLogLevel.DEBUG);
+        public static readonly LogLevel INFO = new LogLevel(InternalLogLevel.INFO);
+        public static readonly LogLevel WARN = new LogLevel(InternalLogLevel.WARN);
+        public static readonly LogLevel ERROR = new LogLevel(InternalLogLevel.ERROR);
+
+        private readonly InternalLogLevel internalLevel;
+
+        private LogLevel()
+        {
+        }
+
+        private LogLevel(InternalLogLevel internalLevel)
+        {
+            this.internalLevel = internalLevel;
+        }
+
+        public InternalLogLevel ToInternalLevel()
+        {
+            return internalLevel;
+        }
+    }
+}
