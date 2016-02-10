89189427a886b2ff456b64269b309d6809441649
mgortman <mgortman@microsoft.com>
2016-02-10 01:31:16 -0800

    Enables setting options specific to DefaultSocketChannelConfiguration (fix #68)
    Motivation:
    Fix a bug that breaks setting options specific to socket-based channel implementations
    
    Modifications:
    Added SetOption overload accepting socket-specific options to DefaultSocketChannelConfiguration
    
    Result:
    It is possible to modify send/receive buffer size and other settings through SetOption on Bootstrap.

--- src/DotNetty.Transport/Channels/Sockets/ISocketChannelConfiguration.cs
+++ src/DotNetty.Transport/Channels/Sockets/ISocketChannelConfiguration.cs
@@ -5,5 +5,18 @@ namespace DotNetty.Transport.Channels.Sockets
 {
     public interface ISocketChannelConfiguration : IChannelConfiguration
     {
+        bool AllowHalfClosure { get; set; }
+
+        int ReceiveBufferSize { get; set; }
+
+        int SendBufferSize { get; set; }
+
+        int Linger { get; set; }
+
+        bool KeepAlive { get; set; }
+
+        bool ReuseAddress { get; set; }
+
+        bool TcpNoDelay { get; set; }
     }
 }
