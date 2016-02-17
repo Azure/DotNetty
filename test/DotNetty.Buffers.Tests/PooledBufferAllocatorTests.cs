1f77e0cc64606aa1f7c0aad7ff481bb035371ab5
mgortman <mgortman@microsoft.com>
2016-02-17 00:08:15 -0800

    Fixes pooled buffer's AdjustCapacity to properly set the length
    Motivation:
    Fix a bug #71.
    
    Modifications:
    Set length property of a PooledByteBuffer in case of a switch of underlying array to unpooled.
    
    Results:
    PooledByteBufferAllocator can work with buffers bigger than pooled buffer's original capacity.

---
+++ test/DotNetty.Buffers.Tests/PooledBufferAllocatorTests.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace DotNetty.Buffers.Tests
+{
+    using Xunit;
+
+    public class PooledBufferAllocatorTests
+    {
+        [Theory]
+        [InlineData(16 * 1024, 10, new[] {16 * 1024 - 100, 8 * 1024})]
+        [InlineData(16 * 1024, 0, new[] { 16 * 1024 - 100, 8 * 1024 })]
+        [InlineData(1024, 2 * 1024, new[] { 16 * 1024 - 100, 8 * 1024 })]
+        [InlineData(1024, 0, new[] { 1024, 1 })]
+        [InlineData(1024, 0, new[] { 1024, 0, 10 * 1024 })]
+        public void PooledBufferGrowTest(int bufferSize, int startSize, int[] writeSizes)
+        {
+            var alloc = new PooledByteBufferAllocator(bufferSize, int.MaxValue);
+            IByteBuffer buffer = alloc.Buffer(startSize);
+            int wrote = 0;
+            foreach (int size in writeSizes)
+            {
+                buffer.WriteBytes(Unpooled.WrappedBuffer(new byte[size]));
+                wrote += size;
+            }
+
+            Assert.Equal(wrote, buffer.ReadableBytes);
+        }
+    }
+}
