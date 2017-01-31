1e5fc8a523aa678a8b236b61358ed679e0feb94b
Artur Laksberg <arturl@microsoft.com>
2017-01-30 16:06:56 -0800

    Remove dependency on System.Diagnostics.Process

--- src/DotNetty.Common/Internal/DefaultPlatformImplementation.cs
+++ src/DotNetty.Common/Internal/DefaultPlatformImplementation.cs
@@ -3,11 +3,28 @@
 
 namespace DotNetty.Common.Internal
 {
-    using System.Diagnostics;
+    using System;
+    using System.Diagnostics.Contracts;
 
     class DefaultPlatform : IPlatform
     {
-        int IPlatform.GetCurrentProcessId() => Process.GetCurrentProcess().Id;
+        int processId;
+
+        public DefaultPlatform()
+        {
+            // We cannot use System.Diagnostics.Process.GetCurrentProcess here because System.Diagnostics.Process
+            // is not part of .Net Standard 1.3. Since a time-seeded random number is already used in DefaultChannelId 
+            // (the only consumer of this API), we'll use the first 4 bytes of a UUID for better entropy
+
+            var processGuid = Guid.NewGuid();
+            processId = BitConverter.ToInt32(processGuid.ToByteArray(), 0);
+
+            // DotNetty expects process id to be no greater than 0x400000, so clear this higher bits:
+            processId = processId & 0x3FFFFF;
+            Contract.Assert(processId <= 0x400000);
+        }
+
+        int IPlatform.GetCurrentProcessId() => processId;
 
         byte[] IPlatform.GetDefaultDeviceId() => MacAddressUtil.GetBestAvailableMac();
     }
