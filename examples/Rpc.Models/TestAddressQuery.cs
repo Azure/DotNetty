4ded8921d64441c1fcaabe3c209291ab5eda0648
caozhiyuan <caozhiyuan@suiyi.com.cn>
2017-02-05 14:58:14 +0800

    Test

---
+++ examples/Rpc.Models/TestAddressQuery.cs
@@ -0,0 +1,14 @@
+﻿namespace Rpc.Models
+{
+    using DotNetty.Rpc.Service;
+
+    public class TestAddressQuery : AbsMessage<AddressInfo>
+    {
+
+    }
+
+    public class AddressInfo : IResult
+    {
+
+    }
+}
--- examples/Rpc.Services/Test/TestEventHandler.cs
+++ examples/Rpc.Services/Test/TestEventHandler.cs
@@ -1,12 +1,22 @@
 ﻿namespace Rpc.Services.Test
 {
+    using System;
     using DotNetty.Rpc.Service;
     using Rpc.Models;
     using System.Threading.Tasks;
 
     public class TestEventHandler: EventHandlerImpl
     {
-        protected override void InitializeComponents() => this.AddEventListener<TestCityQuery>(this.Handler);
+        protected override void InitializeComponents()
+        {
+            this.AddEventListener<TestCityQuery>(this.Handler);
+            this.AddEventListener<TestAddressQuery>(this.Handler);
+        }
+
+        private Task<TestAddressQuery> Handler(TestAddressQuery eventData)
+        {
+            throw new NotImplementedException();
+        }
 
         private Task<TestCityQuery> Handler(TestCityQuery eventData)
         {
