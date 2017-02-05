0317dd8ef8e7070016e6d483e30b9f652bb12189
caozhiyuan <caozhiyuan@suiyi.com.cn>
2017-02-05 11:25:01 +0800

    examples/Rpc.Client examples/Rpc.Server

---
+++ examples/Rpc.Server/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rpc.Server
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
