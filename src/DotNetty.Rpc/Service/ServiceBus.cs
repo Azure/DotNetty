568f703d013115c13c3c5847d131137f1e45602b
caozhiyuan <czy@qq.com>
2017-02-05 21:05:38 +0800

    ServiceBus DynamicMethods

--- src/DotNetty.Rpc/Service/ServiceBus.cs
+++ src/DotNetty.Rpc/Service/ServiceBus.cs
@@ -3,6 +3,8 @@ namespace DotNetty.Rpc.Service
 {
     using System.Collections.Concurrent;
     using System;
+    using System.Collections.Generic;
+    using System.Linq.Expressions;
     using System.Threading.Tasks;
 
     internal class ServiceBus
@@ -14,26 +16,32 @@ namespace DotNetty.Rpc.Service
             get { return Instance0; }
         }
 
-        internal ConcurrentDictionary<Type, Delegate> Handlers { get; private set; } = new ConcurrentDictionary<Type, Delegate>();
+        internal ConcurrentDictionary<Type, Func<object,Task>> DynamicMethods { get; private set; } = new ConcurrentDictionary<Type, Func<object, Task>>();
 
         internal void Subscribe<T>(Listener<T> subscriber) where T : IMessage
         {
-            Delegate handler;
+            Func<object, Task> func;
             Type type = typeof(T);
-            if (this.Handlers.TryGetValue(type, out handler))
+            if (this.DynamicMethods.TryGetValue(type, out func))
             {
                 throw new Exception("Mutil Listener For this IMessage");
             }
-            this.Handlers.TryAdd(typeof(T), subscriber);
+
+            ParameterExpression r = Expression.Parameter(typeof(object));
+            Expression c = Expression.Call(Expression.Constant(subscriber.Target), subscriber.Method, Expression.Convert(r, type));
+
+            IEnumerable<ParameterExpression> parameters = new[] { r };
+            func = Expression.Lambda<Func<object, Task>>(c, parameters).Compile();
+            this.DynamicMethods.TryAdd(type, func);
         }
 
         internal async Task<object> Publish(object eventArgs)
         {
-            Delegate handler;
+            Func<object, Task> handler;
             Type type = eventArgs.GetType();
-            if (this.Handlers.TryGetValue(type, out handler))
+            if (this.DynamicMethods.TryGetValue(type, out handler))
             {
-                await (Task)handler.DynamicInvoke(eventArgs);
+                await handler(eventArgs);
                 return ((IMessage)eventArgs).ReturnValue;
             }
             throw new NotImplementedException(type.Name);
