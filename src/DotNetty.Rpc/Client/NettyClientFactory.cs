92410e6921979bb4fe5c8ad00c4302135b7c7d24
caozhiyuan <caozhiyuan@suiyi.com.cn>
2017-02-07 08:20:59 +0800

    ServiceClientAsyncLockMap

--- src/DotNetty.Rpc/Client/NettyClientFactory.cs
+++ src/DotNetty.Rpc/Client/NettyClientFactory.cs
@@ -5,10 +5,12 @@ namespace DotNetty.Rpc.Client
 {
     using System.Collections.Concurrent;
     using System.Net;
+    using System.Threading;
 
     public class NettyClientFactory
     {
         private static readonly ConcurrentDictionary<string, NettyClient> ServiceClientMap = new ConcurrentDictionary<string, NettyClient>();
+        private static readonly ConcurrentDictionary<string, SemaphoreSlim> ServiceClientAsyncLockMap = new ConcurrentDictionary<string, SemaphoreSlim>();
 
         public static async Task<NettyClient> Get(string serverAddress)
         {
@@ -17,12 +19,26 @@ namespace DotNetty.Rpc.Client
             if (client != null)
                 return client;
 
-            var newClient = new NettyClient();
-            string[] array = serverAddress.Split(':');
-            string host = array[0];
-            int port = Convert.ToInt32(array[1]);
-            EndPoint remotePeer = new IPEndPoint(IPAddress.Parse(host).MapToIPv6(), port);
-            await newClient.Connect(remotePeer);
+            SemaphoreSlim slim = ServiceClientAsyncLockMap.GetOrAdd(serverAddress, new SemaphoreSlim(1, 1));
+            await slim.WaitAsync();
+
+            NettyClient newClient;
+            try
+            {
+                newClient = new NettyClient();
+                string[] array = serverAddress.Split(':');
+                string host = array[0];
+                int port = Convert.ToInt32(array[1]);
+                EndPoint remotePeer = new IPEndPoint(IPAddress.Parse(host).MapToIPv6(), port);
+                await newClient.Connect(remotePeer);
+            }
+            finally
+            {
+                slim.Release();
+            }
+
+            if (newClient == null)
+                throw new NullReferenceException();
 
             ServiceClientMap.TryAdd(serverAddress, newClient);
             return newClient;
