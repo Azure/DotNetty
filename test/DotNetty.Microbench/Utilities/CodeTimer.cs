099374d757fc4c22e47dfae2dbc44779e14eb994
ravengerUA <yurisikorsky@gmail.com>
2017-02-16 23:16:55 +0200

    Downgraded to net45

--- test/DotNetty.Microbench/Utilities/CodeTimer.cs
+++ test/DotNetty.Microbench/Utilities/CodeTimer.cs
@@ -39,7 +39,7 @@ namespace DotNetty.Microbench.Utilities
             // Get the time before returning so that any code above doesn't 
             // impact the time.
             this.startTime = Stopwatch.GetTimestamp();
-#if NET451
+#if NET45
             var handle = Process.GetCurrentProcess().Handle;
 #else
             var handle = Process.GetCurrentProcess().SafeHandle.DangerousGetHandle();
@@ -121,7 +121,7 @@ namespace DotNetty.Microbench.Utilities
 
         public void Dispose()
         {
-#if NET451
+#if NET45
             var handle = Process.GetCurrentProcess().Handle;
 #else
             var handle = Process.GetCurrentProcess().SafeHandle.DangerousGetHandle();
