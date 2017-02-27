6a46abf962795fbc98cbb1893759e03164ba637c
caozhiyuan <caozhiyuan@suiyi.com.cn>
2017-02-27 16:42:16 +0800

    u

--- src/DotNetty.Rpc/Client/NettyClient.cs
+++ src/DotNetty.Rpc/Client/NettyClient.cs
@@ -84,8 +84,15 @@ namespace DotNetty.Rpc.Client
                 if (n.IsFaulted || n.IsCanceled)
                 {
                     Logger.Info("NettyClient connected to {} failed", socketAddress);
-                    IChannel channel0 = this.clientRpcHandler.GetChannel();
-                    channel0.EventLoop.Schedule(_ => this.DoConnect((EndPoint)_), socketAddress, TimeSpan.FromMilliseconds(1000));
+                    if (this.clientRpcHandler != null)
+                    {
+                        IChannel channel0 = this.clientRpcHandler.GetChannel();
+                        channel0.EventLoop.Schedule(_ => this.DoConnect((EndPoint)_), socketAddress, TimeSpan.FromMilliseconds(1000));
+                    }
+                    else
+                    {
+                        WorkerGroup.GetNext().Schedule(_ => this.DoConnect((EndPoint)_), socketAddress, TimeSpan.FromMilliseconds(1000));
+                    }
                 }
                 else
                 {
