46bfe265c9fedfd4fb83001dceeb278c560177f3
caozhiyuan <caozhiyuan@suiyi.com.cn>
2017-08-21 18:39:20 +0800

    dev

--- examples/SocketAsyncServer/Program.cs
+++ examples/SocketAsyncServer/Program.cs
@@ -5,12 +5,12 @@ namespace SocketAsyncServer
 {
     public static class Program
     {
-        public static void Main(String[] args)
+        public static void Main(string[] args)
         {
             try
             {
                 int port = 9900;
-                SocketListener sl = new SocketListener();
+                var sl = new SocketListener();
                 sl.Start(port);
 
                 Console.WriteLine("Server listening on port {0}. Press any key to terminate the server process...", port);
@@ -19,26 +19,10 @@ namespace SocketAsyncServer
                 sl.Stop();
 
             }
-            catch (IndexOutOfRangeException)
-            {
-                PrintUsage();
-            }
-            catch (FormatException)
-            {
-                PrintUsage();
-            }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());
             }
-        }
-
-        private static void PrintUsage()
-        {
-            Console.WriteLine("Usage: SocketAsyncServer <port> [numConnections] [bufferSize].");
-            Console.WriteLine("\t<port> Numeric value for the listening TCP port.");
-            Console.WriteLine("\t[numConnections] Numeric value for the maximum number of incoming connections.");
-            Console.WriteLine("\t[bufferSize] Numeric value for the buffer size of incoming connections.");
         }
     }
 }
