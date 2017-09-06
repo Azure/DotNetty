1eae16efc76b96e27be518858055a95afcfcdaec
caozhiyuan <caozhiyuan@suiyi.com.cn>
2017-09-06 08:30:52 +0800

    cc

--- examples/Rpc.Client/Program.cs
+++ examples/Rpc.Client/Program.cs
@@ -39,7 +39,7 @@ namespace Rpc.Client
 
         public static void Test()
         {
-            string serverAddress = "192.168.139.132:9008";
+            string serverAddress = "10.1.4.204:9008";
 
             var sw = new Stopwatch();
             sw.Start();
