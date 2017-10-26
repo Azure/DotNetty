5d696b3d82f1fa86e09c7496b54362bef0307868
caozhiyuan <caozhiyuan@suiyi.com.cn>
2017-10-26 14:53:58 +0800

    cs

--- src/DotNetty.Rpc/Server/RpcHandler.cs
+++ src/DotNetty.Rpc/Server/RpcHandler.cs
@@ -6,7 +6,6 @@
     using DotNetty.Codecs;
     using DotNetty.Common.Internal.Logging;
     using DotNetty.Handlers.Timeout;
-    using DotNetty.Rpc.Exceptions;
     using DotNetty.Rpc.Protocol;
     using DotNetty.Rpc.Service;
     using DotNetty.Transport.Channels;
