58a82a0e3c336ae7e9b771edf01666a2b06008a3
Max Gortman <mgortman@microsoft.com>
2017-11-06 16:01:29 -0800

    set version to 0.4.7

--- src/shared/SharedAssemblyInfo.cs
+++ src/shared/SharedAssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Reflection;
 
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("DotNetty")]
-[assembly: AssemblyVersion("0.4.6")]
-[assembly: AssemblyFileVersion("0.4.6")]
+[assembly: AssemblyVersion("0.4.7")]
+[assembly: AssemblyFileVersion("0.4.7")]
 [assembly: AssemblyCopyright("(c) Microsoft 2015 - 2017")]
 
