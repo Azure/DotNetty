52b7a75d67ced27b4e05c1b354a629c45ec4e98c
SilverFox <yyjdelete@126.com>
2018-03-14 21:44:55 +0800

    Better inline some methods in AbstractByteBuffer

--- src/DotNetty.Buffers/ThrowHelper.cs
+++ src/DotNetty.Buffers/ThrowHelper.cs
@@ -4,15 +4,35 @@
 namespace DotNetty.Buffers
 {
     using System;
+    using System.Runtime.CompilerServices;
 
     static class ThrowHelper
     {
         public static void ThrowIndexOutOfRangeException(string message) => throw new IndexOutOfRangeException(message);
 
-        public static void ThrowIllegalReferenceCountException(int count = 0) => throw new IllegalReferenceCountException(count);
+        public static void ThrowIndexOutOfRangeException(string format, int index, int length, int capacity) => throw CreateIndexOutOfRangeException(format, index, length, capacity);
 
-        public static void ThrowArgumentNullException(string message) => throw new ArgumentNullException(message);
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        static Exception CreateIndexOutOfRangeException(string format, int index, int length, int capacity) => new IndexOutOfRangeException(string.Format(format, index, length, capacity));
+
+        public static void ThrowIllegalReferenceCountException(int count = 0) => throw CreateIllegalReferenceCountException(count);
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        static Exception CreateIllegalReferenceCountException(int count) => throw new IllegalReferenceCountException(count);
+
+        public static void ThrowArgumentNullException(string name) => throw new ArgumentNullException(name);
 
         public static void ThrowArgumentOutOfRangeException(string name, string message) => throw new ArgumentOutOfRangeException(name, message);
+
+        public static void ThrowArgumentOutOfRangeException_LessThanZero(string name, int value)
+        {
+            throw CreateArgumentOutOfRangeException_LessThanZero(name, value);
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        static Exception CreateArgumentOutOfRangeException_LessThanZero(string name, int value)
+        {
+            return new ArgumentOutOfRangeException(name, $"{name}: {value} (expected: >= 0)");
+        }
     }
 }
