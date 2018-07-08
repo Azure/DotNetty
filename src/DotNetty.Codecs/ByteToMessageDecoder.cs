f9b86a0a6748e2266d8ab86929659be25e2a22a6
SilverFox <yyjdelete@126.com>
2018-07-08 13:27:29 +0800

    Switch back to `DiscardSomeReadBytes` since it's avaliable

--- src/DotNetty.Codecs/ByteToMessageDecoder.cs
+++ src/DotNetty.Codecs/ByteToMessageDecoder.cs
@@ -249,7 +249,7 @@ namespace DotNetty.Codecs
                 // See:
                 // - https://github.com/netty/netty/issues/2327
                 // - https://github.com/netty/netty/issues/1764
-                this.cumulation.DiscardReadBytes(); // todo: use discardSomeReadBytes
+                this.cumulation.DiscardSomeReadBytes();
             }
         }
 
